cd20adc462e538dcf562668a08ca6346645d9aa1
Peter Phan <pphan@hashicorp.com>
2020-08-19 13:00:28 -0700

    update

--- main.tf
+++ main.tf
@@ -16,14 +16,14 @@ provider "aws" {
 provider "template" {
 }
 
-resource "aws_iam_user" "circleci" {
-  name = var.user
-  path = "/system/"
-}
+# resource "aws_iam_user" "circleci" {
+#   name = var.user
+#   path = "/system/"
+# }
 
-resource "aws_iam_access_key" "circleci" {
-  user = aws_iam_user.circleci.name
-}
+# resource "aws_iam_access_key" "circleci" {
+#   user = aws_iam_user.circleci.name
+# }
 
 data "template_file" "circleci_policy" {
   template = file("circleci_s3_access.tpl.json")
@@ -37,11 +37,11 @@ resource "local_file" "circle_credentials" {
   content  = "${aws_iam_access_key.circleci.id}\n${aws_iam_access_key.circleci.secret}"
 }
 
-resource "aws_iam_user_policy" "circleci" {
-  name   = "AllowCircleCI"
-  user   = aws_iam_user.circleci.name
-  policy = data.template_file.circleci_policy.rendered
-}
+# resource "aws_iam_user_policy" "circleci" {
+#   name   = "AllowCircleCI"
+#   user   = aws_iam_user.circleci.name
+#   policy = data.template_file.circleci_policy.rendered
+# }
 
 resource "aws_s3_bucket" "portfolio" {
   tags = {
